7ed67eb0de2cbe1b394647e0b7e80fde83755dfe
cbteeple <cbteeple@g.harvard.edu>
2017-11-27 11:12:32 -0500

    adding a routine

--- physicalRobot/testingVisualization.m
+++ physicalRobot/testingVisualization.m
@@ -1,10 +1,6 @@
 
 %Add "model" folder to the path to give us access to all of those functions
-    thisDir  = pwd;
-    idcs   = strfind(thisDir,'\');
-    modelDir = thisDir(1:idcs(end)-1);
-    modelDir = [modelDir,'\model'];
-    addpath(genpath(modelDir));
+    addModelPaths()
     
     
 %Plot the Visualization:
