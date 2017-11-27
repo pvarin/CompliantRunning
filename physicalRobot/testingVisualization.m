beb0688babd837db935328a2be483d74db282046
cbteeple <cbteeple@g.harvard.edu>
2017-11-27 10:58:11 -0500

    Adding testingVisulization

---
+++ physicalRobot/testingVisualization.m
@@ -0,0 +1,16 @@
+
+%Add "vis" folder to the path to give us access to those functions
+    thisDir  = pwd;
+    idcs   = strfind(thisDir,'\');
+    modelDir = thisDir(1:idcs(end)-1);
+    modelDir = [modelDir,'\model'];
+    addpath(genpath(modelDir));
+    
+    
+%Plot the Visualization:
+    q = example_kinematic_trajectory;
+    p = example_parameters;
+    t = linspace(0,2,size(q,2));
+    animate_trajectory(t,q,p);
+    
+    %test_vis_kinematic_traj
