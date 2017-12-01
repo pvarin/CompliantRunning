5ed2d26856bf301cd71df47ab3c8fdfe4a97877f
cbteeple <cbteeple@g.harvard.edu>
2017-12-01 15:53:25 -0500

    Finished cauculating the inertias, and put everyting into the "true_parameters.m" file

--- model/true_parameters.m
+++ model/true_parameters.m
@@ -3,12 +3,12 @@ function p = true_parameters
     p.g = 9.81;  % keep this as a parameter, so we can debug the dynamics in zero gravity
 
     % morphology
-    p.l1 = 0.2;     % l1 (distance from first motor to second motor)
-    p.l2 = 0.75;    % l2 hip to knee
-    p.l3 = 1.25;    % l3 knee to ankle
-    p.l4 = 0.3;     % hip to hip2
-    p.l5 = 0.1;     % uncompressed distance from ankle to foot tip
-    p.phi = -0.5;   % angle between hip1 and hip2  
+    p.l1 = 0.0552;     % l1 (distance from first motor to second motor)
+    p.l2 = 0.0889;    % l2 hip to knee
+    p.l3 = 0.2032;    % l3 knee to ankle
+    p.l4 = 0.0762;     % hip to hip2
+    p.l5 = 0.1016;     % uncompressed distance from ankle to foot tip
+    p.phi = -pi/4;   % angle between hip1 and hip2  
     p.k = 1000;
     p.k_stop = 3000; % stiff spring for hard stop
     p.x_damping = 0;
@@ -18,31 +18,32 @@ function p = true_parameters
     p.l_damping = 30;
 
     % center of mass position
-    p.body_com_x = 0.1;         % position of body COM relative to body frame
-    p.body_com_y = 0.1; 
-    p.hip_com_x = -0.1;         % position of hip COM relative to hip frame
-    p.hip_com_y = -0.1;
+    p.body_com_x = 0;         % position of body COM relative to body frame
+    p.body_com_y = 0;
+    p.hip_com_x = 0.0031;         % position of hip COM relative to hip frame
+    p.hip_com_y = -0.0374;   
     p.upper_femur_com_x = 0.0;  % position of upper COM relative to knee frame
-    p.upper_femur_com_y = 0.325;
+    p.upper_femur_com_y = 0.0889-0.05325;
     p.lower_femur_com_x = 0.0;  % position of lower femur COM relative to knee2 frame
-    p.lower_femur_com_y = 0.325;
+    p.lower_femur_com_y = 0.04445;
     p.leg_com_x = 0.0;          % position of leg COM relative to ankle frame
-    p.leg_com_y = 0.75;
+    p.leg_com_y = 0.2032-0.12534-.0254;
     p.foot_com_x = 0;           % position of the foot COM relative to the foot frame
-    p.foot_com_y = 0.05;
+    p.foot_com_y = 0.1016/2;
 
     % masses and inertias
-    p.body_mass = 1;        % mass of body
-    p.hip_mass = 1;         % mass of hip
-    p.upper_femur_mass = 1; % mass of upper femur
-    p.lower_femur_mass = 1; % mass of lower femur
-    p.ankle_mass = 1;       % mass of ankle
-    p.foot_mass = .1;       % mass of foot
+    p.body_mass = 0;                % mass of body <<<<<<<<< Not sure of this yet
+    p.hip_mass = 0.5470;            % mass of hip
+    p.upper_femur_mass = 0.0426;    % mass of upper femur
+    p.lower_femur_mass = 0.0225;    % mass of lower femur
+    p.ankle_mass = 0.0851;          % mass of ankle
+    p.foot_mass = 0.00246;               % mass of foot <<<<<<<<< Need to get this
     
-    p.I_body = 1;           % mass of body
-    p.I_hip = 1;            % mass of hip
-    p.I_upper_femur = 1;    % mass of upper femur
-    p.I_lower_femur = 1;    % mass of lower femur
-    p.I_ankle = 1;          % mass of ankle
-    p.I_foot = .001;         % mass of foot
+   
+    p.I_body = 0;                   % inertia of body
+    p.I_hip = 9.2587e-04;           % inertia of hip
+    p.I_upper_femur = 5.281e-5;     % inertia of upper femur
+    p.I_lower_femur = 2.433e-5;     % inertia of lower femur
+    p.I_ankle = 4.503e-4;           % inertia of ankle
+    p.I_foot = 2.1301e-06;          % inertia of foot
 end
--- physicalRobot/MassSpecs/HipCalcs.m
+++ physicalRobot/MassSpecs/HipCalcs.m
@@ -1,8 +1,10 @@
+%% Calculate the COM
+
 clc
 close all
 
-m_motor = 0.215;
-m_plastic = 0.1170;
+m_mot = 0.215;
+m_p = 0.1170;
 
 x_A = [0;0];
 x_B = [-0.03903; 0.03903];
@@ -12,11 +14,11 @@ x_S1 = x_A+[0;-0.007];
 x_S2 = x_B+[-0.007;0];
 
 
-m_total = 2*m_motor + m_plastic
+m_total = 2*m_mot + m_p
 
-x_cm = (m_motor.*x_A + m_motor.*x_B + m_plastic.*x_C)/(m_total)
+x_cm = (m_mot.*x_A + m_mot.*x_B + m_p.*x_C)/(m_total)
 
-theta = pi/4;
+theta = -pi/4;
 R=[cos(theta), -sin(theta); sin(theta), cos(theta)];
 
 c_cm_adj = R* (x_cm + [0; 0.007] - [-0.04603; 0.04603])
@@ -44,3 +46,30 @@ axis equal
 ax.ColorOrderIndex = 1;
 plot([x_S1(1), x_S2(1)], [x_S1(2), x_S2(2)], 'k--')
 hold off
+
+
+%% Calculate Inertia
+
+r_m = 0.0348;
+I_m = (1/2)* m_mot*r_m.^2;
+
+r_mA = norm(x_A-x_cm);
+r_mB = norm(x_B-x_cm);
+
+r_p = norm(x_C-x_cm);
+I_p = 2.834e-4;
+
+
+
+I_tot = I_p + m_p*r_p^2 + 2*I_m + (r_mA^2 + r_mB^2).*m_mot
+
+
+
+%% FOOT:
+
+r_foot=0.0047625;
+h_foot = 0.1016;
+m_foot = 0.00246;
+
+I_foot= m_foot/(12) *(3*r_foot^2 + h_foot^2)
+
